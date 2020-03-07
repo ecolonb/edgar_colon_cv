3fc08d9b1badc1fa7d9cc7cda68fa6179e702a1a
Edgar Colón Barrientos <edgar@envioclick.com>
2020-03-07 10:12:56 -0600

    Change config

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
-require('./config/config');
 const express = require('express');
 const app = new express();
-const { Routes: routes } = require('./routes/web');
-const port = process.env.PORT;
+const {
+    Routes: routes
+} = require('./routes/web');
+
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use('/', routes);
 app.get('*', function(req, res) {
-  res.redirect('/');
+    res.redirect('/');
 });
 app.listen(port, () => {
-  console.log('Server on: http://localhost:' + port);
+    console.log('Server on: http://localhost:' + port);
 });
